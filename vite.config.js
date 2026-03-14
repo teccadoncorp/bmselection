7c12f831e1d370768f19364ea38e51304de12275
teccadoncorp <teccadoncorp@gmail.com>
2026-03-14 01:57:50 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: { '@': '/src' },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
   },
 })
