6e14e807cba2ae222f19b284114941ef84b3622a
teccadoncorp <teccadoncorp@gmail.com>
2026-03-14 02:24:56 -0600

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api'
+const BASE_URL = 'https://bmselection.pythonanywhere.com/api'
 
 const api = axios.create({
   baseURL: BASE_URL,
